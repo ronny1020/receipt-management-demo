384c60ec6f8f65ac68b31444825912029ae4c5ef
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2021-12-30 17:58:22 +0800

    style(eslint): init eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,53 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Recommended IDE setup:
+    <a
+      href="https://code.visualstudio.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      VSCode
+    </a>
+    +
+    <a
+      href="https://github.com/johnsoncodehk/volar"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Volar
+    </a>
+  </p>
+
+  <p>
+    See
+    <code>README.md</code>
+    for more information.
+  </p>
+
+  <p>
+    <a
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Vite Docs
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank" rel="noopener noreferrer">
+      Vue 3 Docs
+    </a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code>
+    to test hot module replacement.
+  </p>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
 
@@ -6,34 +56,7 @@ defineProps<{ msg: string }>()
 const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
-</template>
-
-<style scoped>
+<style lang="scss" scoped>
 a {
   color: #42b983;
 }
