1dea2ea1857672668b20b1a1820b919bae4d65d7
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2021-12-31 00:22:03 +0800

    style(stylelint): init

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style lang="scss" scoped>
 #app {
+  margin-top: 60px;
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
