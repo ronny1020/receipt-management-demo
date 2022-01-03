407112f8d0d458adeb6a7fd5f248aea865b92ef2
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2022-01-03 14:23:39 +0800

    chore(vite): add '@': path.resolve(__dirname, './src')

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: import('../components/HelloWorld.vue'),
+    component: import('@/components/HelloWorld.vue'),
   },
 ]
 
