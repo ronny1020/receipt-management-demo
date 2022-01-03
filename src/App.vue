04b9e4aa315a7a3fd220f2e9bee6475bca6c01b5
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2022-01-03 15:41:08 +0800

    chore(css): set up basic css

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <router-view />
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped>
-#app {
-  margin-top: 60px;
-  color: #2c3e50;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+<style lang="scss">
+@import '@/styles';
 </style>
