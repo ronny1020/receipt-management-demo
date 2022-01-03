c10be5f3717c2520ae11517bf697f2e450ad0ffe
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2022-01-03 17:31:48 +0800

    feat(invoices): add description

--- src/modules/invoices/views/invoiceList/InvoiceList.vue
+++ src/modules/invoices/views/invoiceList/InvoiceList.vue
@@ -4,6 +4,10 @@
       <MainLogo />
     </IconBase>
   </header>
+  <div class="description">
+    <span class="title">110年12月</span>
+    <span class="subtitle">共 0,000 張 , 總金額 000,000 元</span>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -19,5 +23,27 @@ header {
   width: 100%;
   height: 120px;
   background: #ffce33;
+}
+
+.description {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 70px;
+
+  .title {
+    color: #37375a;
+    font-weight: bold;
+    font-style: normal;
+    font-size: 18px;
+    line-height: 27px;
+  }
+
+  .subtitle {
+    color: #51519b;
+    font-size: 14px;
+    line-height: 21px;
+  }
 }
 </style>
